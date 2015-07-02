1ff09f2b8497c57a2744a306aaa95da3b5c7ad38
chenqitao <chenqitao520@qq.com>
2015-07-02 09:47:09 +0800

    Initial Commit

---
+++ 招标/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  招标
+//
+//  Created by mac chen on 15/7/2.
+//  Copyright (c) 2015年 陈齐涛. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
