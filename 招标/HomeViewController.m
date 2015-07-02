7a0030003365128217a7d7856ba98f2b0141b11e
chenqitao <chenqitao520@qq.com>
2015-07-02 15:58:39 +0800

    引入框架，使用代码自动布局

--- 招标/HomeViewController.m
+++ 招标/HomeViewController.m
@@ -9,17 +9,43 @@
 #import "HomeViewController.h"
 
 @interface HomeViewController ()
+{
+    UIImageView *backgroundImage;//背景图片
+    UITextField *namefield;
+    UITextField *pwfield;
 
+}
 @end
 
 @implementation HomeViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    [self creatUI];
+
+    
+}
+-(void)creatUI{
+    backgroundImage=[UIImageView newAutoLayoutView];
+      [self.view addSubview:backgroundImage];
+    [backgroundImage autoPinEdgeToSuperviewEdge:ALEdgeTop withInset:0];
+    [backgroundImage autoPinEdgeToSuperviewEdge:ALEdgeLeft withInset:0];
+    [backgroundImage autoPinEdgeToSuperviewEdge:ALEdgeRight withInset:0];
+    [backgroundImage  autoPinEdgeToSuperviewEdge:ALEdgeBottom withInset:0];
+     [self SetObfuscationImageView:backgroundImage AndIcon:@"backImage1"];
+ 
+   
 
 
 }
 
+#pragma 设置模糊化图片
+-(void)SetObfuscationImageView:(UIImageView *)imageview AndIcon:(NSString *)Icon{
+
+    [imageview setImageToBlur:[UIImage imageNamed:Icon]
+                   blurRadius:20.0   completionBlock:^(NSError *error){
+                   }];
+}
 
 
 @end
