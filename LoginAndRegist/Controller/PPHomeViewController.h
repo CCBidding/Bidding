8ac3ed445c6e8c1e1799f41a150d764f04c5941f
Appleqq <820566997@qq.com>
2015-08-17 10:52:11 +0800

    注册风格的修改

---
+++ LoginAndRegist/Controller/PPHomeViewController.h
@@ -0,0 +1,13 @@
+//
+//  PPHomeViewController.h
+//  招标
+//
+//  Created by ppan on 15/8/13.
+//  Copyright (c) 2015年 陈齐涛. All rights reserved.
+//
+
+#import "PPBaseViewController.h"
+
+@interface PPHomeViewController : PPBaseViewController
+
+@end
---
+++ LoginAndRegist/Controller/PPHomeViewController.m
@@ -0,0 +1,37 @@
+//
+//  PPHomeViewController.m
+//  招标
+//
+//  Created by ppan on 15/8/13.
+//  Copyright (c) 2015年 陈齐涛. All rights reserved.
+//
+
+#import "PPHomeViewController.h"
+
+@interface PPHomeViewController ()
+
+@end
+
+@implementation PPHomeViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
